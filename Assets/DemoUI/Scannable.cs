136d2f567d6939b25eb91de7867100765f40c53f
Dan Moran <danjohnmoran@gmail.com>
2016-08-23 01:37:46 -0400

    Video Release

---
+++ Assets/DemoUI/Scannable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scannable : MonoBehaviour
+{
+	public Animator UIAnim;
+
+	public void Ping()
+	{
+		UIAnim.SetTrigger("Ping");
+    }
+}
---
+++ Assets/Scanner Effect/ScannerEffectDemo.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ScannerEffectDemo : MonoBehaviour
+{
+	public Transform ScannerOrigin;
+	public Material EffectMaterial;
+	public float ScanDistance;
+
+	private Camera _camera;
+
+	// Demo Code
+	bool _scanning;
+	Scannable[] _scannables;
+
+	void Start()
+	{
+		_scannables = FindObjectsOfType<Scannable>();
+    }
+
+	void Update()
+	{
+		if (_scanning)
+		{
+			ScanDistance += Time.deltaTime * 50;
+			foreach (Scannable s in _scannables)
+			{
+				if (Vector3.Distance(ScannerOrigin.position, s.transform.position) <= ScanDistance)
+					s.Ping();
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			_scanning = true;
+			ScanDistance = 0;
+		}
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				_scanning = true;
+				ScanDistance = 0;
+				ScannerOrigin.position = hit.point;
+			}
+		}
+	}
+	// End Demo Code
+
+	void OnEnable()
+	{
+		_camera = GetComponent<Camera>();
+	}
+
+	[ImageEffectOpaque]
+	void OnRenderImage(RenderTexture src, RenderTexture dst)
+	{
+		EffectMaterial.SetVector("_WorldSpaceScannerPos", ScannerOrigin.position);
+		EffectMaterial.SetFloat("_ScanDistance", ScanDistance);
+		RaycastCornerBlit(src, dst, EffectMaterial);
+	}
+
+	void RaycastCornerBlit(RenderTexture source, RenderTexture dest, Material mat)
+	{
+		// Compute Frustum Corners
+		float camFar = _camera.farClipPlane;
+		float camFov = _camera.fieldOfView;
+		float camAspect = _camera.aspect;
+
+		float fovWHalf = camFov * 0.5f;
+
+		Vector3 toRight = _camera.transform.right * Mathf.Tan(fovWHalf * Mathf.Deg2Rad) * camAspect;
+		Vector3 toTop = _camera.transform.up * Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+
+		Vector3 topLeft = (_camera.transform.forward - toRight + toTop);
+		float camScale = topLeft.magnitude * camFar;
+
+		topLeft.Normalize();
+		topLeft *= camScale;
+
+		Vector3 topRight = (_camera.transform.forward + toRight + toTop);
+		topRight.Normalize();
+		topRight *= camScale;
+
+		Vector3 bottomRight = (_camera.transform.forward + toRight - toTop);
+		bottomRight.Normalize();
+		bottomRight *= camScale;
+
+		Vector3 bottomLeft = (_camera.transform.forward - toRight - toTop);
+		bottomLeft.Normalize();
+		bottomLeft *= camScale;
+
+		// Custom Blit, encoding Frustum Corners as additional Texture Coordinates
+		RenderTexture.active = dest;
+
+		mat.SetTexture("_MainTex", source);
+
+		GL.PushMatrix();
+		GL.LoadOrtho();
+
+		mat.SetPass(0);
+
+		GL.Begin(GL.QUADS);
+
+		GL.MultiTexCoord2(0, 0.0f, 0.0f);
+		GL.MultiTexCoord(1, bottomLeft);
+		GL.Vertex3(0.0f, 0.0f, 0.0f);
+
+		GL.MultiTexCoord2(0, 1.0f, 0.0f);
+		GL.MultiTexCoord(1, bottomRight);
+		GL.Vertex3(1.0f, 0.0f, 0.0f);
+
+		GL.MultiTexCoord2(0, 1.0f, 1.0f);
+		GL.MultiTexCoord(1, topRight);
+		GL.Vertex3(1.0f, 1.0f, 0.0f);
+
+		GL.MultiTexCoord2(0, 0.0f, 1.0f);
+		GL.MultiTexCoord(1, topLeft);
+		GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+		GL.End();
+		GL.PopMatrix();
+	}
+}
